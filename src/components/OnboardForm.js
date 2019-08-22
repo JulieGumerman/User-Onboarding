cdddffa1b42e4ce41032966cbf08f0a18ba1df6a
bodhibrdr <julie.gumerman@gmail.com>
2019-08-22 13:54:30 -0600

    MVP with a couple quirks

--- src/components/OnboardForm.js
+++ src/components/OnboardForm.js
@@ -1,27 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field} from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-function OnboardForm () {
+function OnboardForm ({ errors, touched, values, status}) {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status]);
+        }
+    }, [status]);
+
     return (
         <div>
         <h3>I am the form you have been looking for.</h3>
         <Form>
-            <label>Name:</label>
-            <Field type="text" name="name"/>
-            <label>Email:</label>
-            <Field type="email" name="password"/>
+            <Field type="text" name="name" placeholder="name"/>
+            {touched.name && errors.name && (<p>{errors.name}</p>)}
+            <Field type="email" name="email" placeholder="email"/>
+            {touched.email && errors.email && (<p>{errors.email}</p>)}
+            <Field type="password" name="password" placeholder="password"/>
+            {touched.password && errors.password &&(<p>{errors.password}</p>)}
+            <label> I agree to the terms of service.
+                <Field type="checkbox" name="terms" checked={values.terms} />
+                {touched.terms && errors.terms && (<p>{errors.terms}</p>)}
+            </label>
+            <button type="submit">Submit here.</button>
+
         </Form>
+        {users.map(user => (
+            <div>
+                <h3>{user.name}</h3>
+                <h4>{user.email}</h4>
+                <p>{user.password}</p>
+                <p>{user.name} is a good person. Be like {user.name}.</p>
+            </div>
+        ))}
+
+
         </div>
         
     )
 }
 
 const FormikOnboardForm = withFormik({
-    mapPropsToValues({ name, email }) {
+    mapPropsToValues({ name, email, password, terms }) {
         return {
             name: name || "",
             email: email || "",
+            password: password || "",
+            terms: terms || false
         };
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("You call that an answer????"),
+        email: Yup.string().required("Come on. We gotta contact you!!!"),
+        password: Yup.string().required("Keep it secret. Keep it safe"),
+        terms: Yup.string().required("You gotta be nice. Check the thing")
+    }),
+
+    handleSubmit(values, { setStatus}) {
+        axios
+            .post("https://reqres.in/api/users/", values)
+            .then(res => {
+                console.log(res.data);
+                setStatus(res.data);
+            });
     }
 })(OnboardForm);
 
